2d5a5f2fe03b45177ad9df6f3b6ba674b33d7bd7
clawlynx <clawlynx77@gmail.com>
2025-06-03 11:40:11 +0530

    edit go back fixed

--- client/src/components/products/editProduct/EditProductHeader.jsx
+++ client/src/components/products/editProduct/EditProductHeader.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import GoBack from "../../GoBack";
+import { useParams } from "react-router-dom";
 
 export default function EditProductHeader() {
+  const { id } = useParams();
   return (
     <div className="flex sm justify-between items-center">
       <h4 className="text-xl font-semibold">Edit Product</h4>
-      <GoBack route={"/products/1"} />
+      <GoBack route={`/products/${id}`} />
     </div>
   );
 }
