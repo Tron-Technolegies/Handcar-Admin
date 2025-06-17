6269bf1a63d35363f879804030fa85871ccf51af
clawlynx <clawlynx77@gmail.com>
2025-06-17 10:28:43 +0530

    added new statss on dashboard

--- client/src/components/dashboard/StatsSection.jsx
+++ client/src/components/dashboard/StatsSection.jsx
@@ -5,51 +5,68 @@ import StatElt from "./StatElt";
 import Graph from "./Graph";
 import MostViewedTable from "./MostViewedTable";
 import LatestProducts from "./LatestProducts";
+import useGetAllStats from "../../hooks/dashboard/useGetAllStats";
+import Loading from "../Loading";
 
-const statData1 = [
-  {
-    icon: "/dashboard/icon-1.png",
-    title: "Services",
-    number: "6,580",
-    percent: "5%",
-    up: true,
-    dip: <HiMiniArrowTrendingUp />,
-  },
-  {
-    icon: "/dashboard/icon-2.png",
-    title: "Products Sold",
-    number: "5,679",
-    up: true,
-    percent: "2%",
-    dip: <HiMiniArrowTrendingUp />,
-  },
-  {
-    icon: "/dashboard/icon-3.png",
-    title: "Users",
-    number: "51,801",
-    up: false,
-    percent: "3%",
-    dip: <HiMiniArrowTrendingDown />,
-  },
-];
+// const statData1 = [
+//   {
+//     icon: "/dashboard/icon-1.png",
+//     title: "Services",
+//     number: "6,580",
+//     percent: "5%",
+//     up: true,
+//     dip: <HiMiniArrowTrendingUp />,
+//   },
+//   {
+//     icon: "/dashboard/icon-2.png",
+//     title: "Products Sold",
+//     number: "5,679",
+//     up: true,
+//     percent: "2%",
+//     dip: <HiMiniArrowTrendingUp />,
+//   },
+//   {
+//     icon: "/dashboard/icon-3.png",
+//     title: "Users",
+//     number: "51,801",
+//     up: false,
+//     percent: "3%",
+//     dip: <HiMiniArrowTrendingDown />,
+//   },
+// ];
 
 export default function StatsSection() {
+  const { loading, vendors, products, users, subscriptions } = useGetAllStats();
   return (
     <div>
-      <div className="grid md:grid-cols-3 grid-cols-1 gap-3 place-items-center">
-        {statData1.map((x, index) => (
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="grid md:grid-cols-3 grid-cols-1 gap-3 place-items-center">
           <StatElt
-            key={index}
-            icon={x.icon}
-            title={x.title}
-            number={x.number}
-            dip={x.dip}
-            up={x.up}
-            percent={x.percent}
+            icon={"/dashboard/icon-1.png"}
+            title={"Vendors"}
+            number={vendors}
           />
-        ))}
-      </div>
-      <div className="flex xl:flex-row flex-col gap-2 my-10">
+          <StatElt
+            icon={"/dashboard/icon-2.png"}
+            title={"Products"}
+            number={products}
+          />
+          <StatElt
+            icon={"/dashboard/icon-3.png"}
+            title={"Users"}
+            number={users}
+          />
+          <StatElt
+            icon={"/dashboard/icon-1.png"}
+            title={"Subscriptions"}
+            number={subscriptions}
+          />
+        </div>
+      )}
+
+      {/* <div className="flex xl:flex-row flex-col gap-2 my-10">
         <div className="flex flex-col justify-center gap-5 mx-auto">
           <p className="text-2xl font-semibold text-left w-full">Sales Data</p>
           <Graph />
@@ -58,7 +75,7 @@ export default function StatsSection() {
           <p className="text-2xl font-semibold">Most Viewed</p>
           <MostViewedTable />
         </div>
-      </div>
+      </div> */}
       <div className="flex flex-col gap-5">
         <h5 className="text-2xl font-semibold">Latest Products</h5>
         <LatestProducts />
---
+++ client/src/hooks/dashboard/useGetAllStats.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { base_url } from "../../constants";
+import axios from "axios";
+
+const useGetAllStats = () => {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState(0);
+  const [vendors, setVendors] = useState(0);
+  const [users, setUsers] = useState(0);
+  const [subscriptions, setSubscriptions] = useState(0);
+
+  const getStats = async () => {
+    setLoading(true);
+
+    try {
+      const res = await axios.get(`${base_url}/admin_dashboard`, {
+        withCredentials: true,
+      });
+      const data = res.data;
+      setProducts(data.total_accessories);
+      setUsers(data.total_users);
+      setVendors(data.total_services);
+      setSubscriptions(data.total_subscribers);
+    } catch (err) {
+      console.log(
+        err?.response?.data?.message ||
+          err?.response?.data?.error ||
+          err?.message ||
+          "something went wrong"
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    getStats();
+  }, []);
+
+  const refetch = () => {
+    getStats();
+  };
+
+  return { loading, users, products, vendors, subscriptions, refetch };
+};
+
+export default useGetAllStats;
